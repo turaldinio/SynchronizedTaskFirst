81f44850a6cd1fb487d5926a39747cc58fff3691
Tural Guluev <tural-98@bk.ru>
2022-08-14 14:39:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ProducerConsumer<Car> producerConsumer = new ProducerConsumer<>();
+        Car car = new Car("BMV");
+
+        Thread producer = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                try {
+                    producerConsumer.produce(car);
+                    Thread.sleep(350);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        });
+        producer.start();
+
+        for (int a = 0; a < 5; a++) {
+            Thread consumer = new Thread(() -> {
+                try {
+                    Thread.sleep(800);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                producerConsumer.consume();
+            }, "Покупатель " + a);
+            consumer.start();
+
+        }
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            return;
+        }
+        producer.interrupt();
+
+    }
+}
+
+class ProducerConsumer<T> {
+    private final List<T> buffer = new ArrayList<>();
+
+    public void produce(T value) {
+        synchronized (buffer) {
+
+            buffer.add(value);
+            System.out.println("Производитель добавил машину " + value);
+            buffer.notify();
+        }
+    }
+
+    public void consume() {
+        synchronized (buffer) {
+            String threadName = Thread.currentThread().getName();
+            if (buffer.isEmpty()) {
+                try {
+                    System.out.println(threadName + " ожидает поставки машины");
+                    buffer.wait();
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+            System.out.println(threadName + " покупает машину");
+            buffer.remove(0);
+        }
+    }
+}
+
+class Car {
+    private String carName;
+
+    public Car(String carName) {
+        this.carName = carName;
+    }
+
+    @Override
+    public String toString() {
+        return carName;
+    }
+}
